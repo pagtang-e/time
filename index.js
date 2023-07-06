03a1cf700a8dfc922ec3cd36a16e702d45d967e1
Pagtang <zmamrowicz@zstigliwicepl.onmicrosoft.com>
2023-07-06 22:16:11 +0200

    analog clock

--- index.js
+++ index.js
@@ -1,32 +1,67 @@
 const dayjs = require('dayjs');
 const utc = require('dayjs/plugin/utc');
 const timezone = require('dayjs/plugin/timezone');
+const ordinal = require('ordinal');
 const MicroModal = require('micromodal');
+const indicator = require('ordinal/indicator');
 const MicroModalSubmit1= document.getElementById('submit-Modal-1')
-let coordinates;
-let timer = document.querySelector('H1');
-let timezoneLocal =document.querySelector('p');
-let Date = document.querySelector('h2');
-let city;
-let newTimezone;
-const startTimeLocalInterval = setInterval(startTimeLocal, 1000);
-
 dayjs.extend(utc)
 dayjs.extend(timezone)
 MicroModal.init();
 
+let coordinates;
+let timer = document.querySelector('H1');
+let timezoneLocal =document.querySelector('.TimeZone');
+let Date = document.querySelector('h2');
+let city;
+let Timezone = dayjs.tz.guess();
+let startTimeLocalInterval;
+let secondRotation;
+let minuteRotation;
+let HourRotation;
+let minuteHand = document.querySelector('.hand.minute')
+let secondHand = document.querySelector('.hand.second')
+let hourHand = document.querySelector('.hand.hour')
+
+
 
 getLocalTime();
+getLocalTimeAnalog()
+
 
 function getLocalTime(){
-timezoneLocal.innerText = dayjs.tz.guess();
+  startTimeLocalInterval = setInterval(startTimeLocal, 1000);
+timezoneLocal.innerText = Timezone;
 startTimeLocal()
+
+}
+function getLocalTimeAnalog(){
+  startTimeLocalAnalog()
+  startTimeLocalInterval = setInterval(startTimeLocalAnalog, 1000);
+timezoneLocal.innerText = dayjs.tz.guess();
+
+
+}
+
+function startTimeLocalAnalog(){
+secondRotation = 360/60*dayjs().second();
+minuteRotation = 360/60*dayjs().minute()
+HourRotation = 360/12*(dayjs().hour()%12)+(30 * dayjs().minute()/60);
+HourRotation = (Math.round(HourRotation * 10) / 10);
+hourHand.style.rotate = HourRotation+"deg"  
+minuteHand.style.rotate = minuteRotation+'deg'  
+secondHand.style.rotate = secondRotation+'deg'
+
+
+
+
 }
 
 function startTimeLocal(){
 
-  timer.innerText= dayjs().format('HH:mm');
-  Date.innerText = dayjs().format("dddd, DD of MMMM YYYY")
+  timer.innerText= dayjs().tz(Timezone).format('HH:mm');
+  console.log(Timezone);
+  Date.innerText = dayjs().tz(Timezone).format('dddd, D') + indicator(dayjs().tz(Timezone).date()) + dayjs().tz(Timezone).format(' of MMMM YYYY')
 
 
 
@@ -52,29 +87,21 @@ MicroModalSubmit1.addEventListener('click',() =>{
       .then(data=> setNewTimezone(data))
 
       function setNewTimezone(data){
-        newTimezone=data.timeZoneId
+        Timezone=data.timeZoneId
         
-        getChosenTime()
+        clearInterval(startTimeLocalInterval);
+        getLocalTime()
       }
 
   }
-
-
-
-
-
 })
 
 
-function getChosenTime(){
-  clearInterval(startTimeLocalInterval);
- 
-  timezoneLocal.innerText = newTimezone;
-  startTimeChosen()
-  function startTimeChosen(){
-      timer.innerText= dayjs().tz(newTimezone).format('HH:mm:ss');
-      Date.innerText = dayjs().tz(newTimezone).format("dddd, DD of MMMM YYYY")
-  setTimeout(startTimeChosen, 1000);
-  
-  
-  }}
+
+
+  let clockType = document.getElementById('clockType')
+  clockType.addEventListener('click', ()=>{
+    if(clockType.checked == true){
+      console.log('fjdshkjf');
+    }
+  })
